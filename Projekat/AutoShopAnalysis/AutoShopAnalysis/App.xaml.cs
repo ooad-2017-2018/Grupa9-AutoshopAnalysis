fc946f0ab38f1e52d5e2e727825a4c930bc74757
Cazim Rahic <r.cazim7@gmail.com>
2018-04-05 01:07:43 +0200

    Dodana Forma za login

--- Projekat/AutoShopAnalysis/AutoShopAnalysis/App.xaml.cs
+++ Projekat/AutoShopAnalysis/AutoShopAnalysis/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoShopAnalysis.Forme;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -66,7 +67,7 @@ namespace AutoShopAnalysis
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Pocetna), e.Arguments);
+                    rootFrame.Navigate(typeof(LogInPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
