f504d788e90670e9b2ed154c8af93405f6f22e60
nejiram <37294883+nejiram@users.noreply.github.com>
2018-06-06 03:05:08 +0200

    klase asp

--- Projekat/AutoShop_ASP.NET/AutoShopAspNet/AutoShopAspNet/Models/Korisnik.cs
+++ Projekat/AutoShop_ASP.NET/AutoShopAspNet/AutoShopAspNet/Models/Korisnik.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoShopAspNet.Models
 {
     public class Korisnik
     {
-        public int ID { get; set; }
+        public int korisnikId { get; set; }
         public string ime { get; set; }
         public string prezime { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string telefon { get; set; }
-        //racun
+        public BankovniRacun idBankovniRacun { get; set; }
+
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual ICollection<Automobil> Automobil { get; set; }
 
     }
 }
